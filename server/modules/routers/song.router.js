3ac1c1b69ae1c05c3e3aaf24232f15acc1722c0f
Ben Hacker <bhacker1995@gmail.com>
2018-06-07 11:03:00 -0500

    GET and DELETE reqs done

--- server/modules/routers/song.router.js
+++ server/modules/routers/song.router.js
@@ -1,9 +1,21 @@
 const express = require( 'express' );
 const router = express.Router();
 
+// Get our connection to the database
+const pool = require( '../pool.js');
+
 router.get( '/', ( req, res ) => {
     console.log( 'In song.router GET' );
-    res.sendStatus( 200 );
+    // Build a string for query
+    const queryText = 'SELECT * FROM songs';
+    pool.query( queryText )
+    .then( ( result ) => {
+        res.send( result.rows );
+    })
+    .catch( ( error ) => {
+        console.log( `Error getting all songs: ${ error }` );
+        res.sendStatus( 500 );
+    });
 });
 
 router.post( '/', ( req, res ) => {
@@ -16,9 +28,18 @@ router.put( '/', ( req, res ) => {
     res.sendStatus( 200 );
 });
 
-router.delete( '/', ( req, res ) => {
+router.delete( '/:id', ( req, res ) => {
+    const id = req.params.id;
     console.log( 'In song.router DELETE to delete' );
-    res.sendStatus( 200 );
+    const queryText = 'DELETE FROM songs WHERE id=$1';
+    pool.query( queryText, [ id ] )
+    .then( ( results ) => {
+        console.log( `Successful delete of song ${ results }` );
+        res.sendStatus( 200 );
+    }).catch( ( error ) => {
+        console.log( `Error deleting songs: ${ error }` );
+        res.sendStatus( 500 );
+    });
 });
 
 module.exports = router;
